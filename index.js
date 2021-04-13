e54bfe719e43cfecb754fe7277d3bc74be8418d3
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-04-12 21:14:50 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (num1 = 2000 , num2 = 100) => num1 / num2;
+
+const square = (parameter1) => parameter1 * parameter1;
+
+const add = (parameter1 , parameter2) => parameter1 + parameter2;
